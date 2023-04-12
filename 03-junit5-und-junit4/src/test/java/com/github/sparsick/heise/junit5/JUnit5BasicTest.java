2246589a6aec1c1c8e1d673fa1e10e89252af2fc
Sandra Parsick <mail@sandra-parsick.de>
2023-04-12 09:50:12 +0200

    add junit5 to junit4 project

---
+++ 03-junit5-und-junit4/src/test/java/com/github/sparsick/heise/junit5/JUnit5BasicTest.java
@@ -0,0 +1,13 @@
+package com.github.sparsick.heise.junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnit5BasicTest {
+
+    @Test
+    void simpleTestMethod(){
+        assertTrue(true);
+    }
+}
