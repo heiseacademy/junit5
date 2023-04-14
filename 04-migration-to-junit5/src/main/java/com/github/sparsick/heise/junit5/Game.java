eac5977324e9858870dcdeeff4e56cfb3cc37297
Sandra Parsick <mail@sandra-parsick.de>
2023-04-14 14:01:11 +0200

    init migration to junit5 with openrewrite

---
+++ 04-migration-to-junit5/src/main/java/com/github/sparsick/heise/junit5/Game.java
@@ -0,0 +1,60 @@
+package com.github.sparsick.heise.junit5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 
+ */
+class Game {
+    
+    private final List<BowlingFrame> bowlingFrames = new ArrayList<>();
+
+    int roll(String allRolls) {
+        fillBowlingFrameList(allRolls);
+        
+        return calculateTotalSumOfGame();
+    }
+
+    private int calculateTotalSumOfGame() {
+        return bowlingFrames.subList(0, 10).stream().mapToInt(bowlingFrame -> bowlingFrame.totalSum()).sum();
+    }
+
+    private void fillBowlingFrameList(String allRolls) {
+        for (int i = 0; i < allRolls.length(); i++) {
+            BowlingFrame bowlingFrame = new BowlingFrame();
+            
+            char firstRoll = allRolls.charAt(i);
+            bowlingFrame.roll(Character.toString(firstRoll));
+            
+            if(!bowlingFrame.isStrike() && rollIsNotTheLastOne(i, allRolls)) {
+                char secondRoll = allRolls.charAt(i + 1);
+                bowlingFrame.roll(Character.toString(secondRoll));
+                
+            }
+            
+            connectFrameWithLastFrame(bowlingFrame);
+            
+            bowlingFrames.add(bowlingFrame);
+          
+            if(!bowlingFrame.isStrike()) {
+                i++;
+            }
+        }
+    }
+
+    private void connectFrameWithLastFrame(BowlingFrame bowlingFrame) {
+        if(!bowlingFrames.isEmpty()) {
+            BowlingFrame lastFrame = bowlingFrames.remove(bowlingFrames.size()-1);
+            lastFrame.nextFrame(bowlingFrame);
+            bowlingFrames.add(lastFrame);
+        }
+    }
+
+    private boolean rollIsNotTheLastOne(int i, String allRolls) {
+        return i < allRolls.length() -1;
+    }
+
+
+}
