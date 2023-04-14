d4b64bfc679bd83577abe50f7f0a75cfab9fd574
Sandra Parsick <mail@sandra-parsick.de>
2023-04-14 16:05:55 +0200

    nested test demo

--- 06-nested-tests/src/test/java/com/github/sparsick/heise/junit5/BowlingFrameTest.java
+++ 06-nested-tests/src/test/java/com/github/sparsick/heise/junit5/BowlingFrameTest.java
@@ -1,5 +1,9 @@
 package com.github.sparsick.heise.junit5;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.IndicativeSentencesGeneration;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,114 +14,121 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  *
  *
  */
+@DisplayName("Bowling Frame")
 class BowlingFrameTest {
 
-    @Test
-    void totalSum_failToKnockAll() {
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("1");
-        frameUnderTest.roll("1");
+    @Nested
+    @DisplayName("Berechne die Gesamtsumme")
+    class TotalSum {
+        @Test
+        @DisplayName("wenn nicht alle Pins umgefallen sind")
+        void failToKnockAll() {
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("1");
+            frameUnderTest.roll("1");
 
-        int totalSum = frameUnderTest.totalSum();
+            int totalSum = frameUnderTest.totalSum();
 
-        assertEquals(2, totalSum);
+            assertEquals(2, totalSum);
+        }
+
+        @Test
+        void secondFailToKnockAll() {
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("2");
+            frameUnderTest.roll("2");
+
+            int totalSum = frameUnderTest.totalSum();
+
+            assertEquals(4, totalSum);
+        }
+
+        @Test
+        void failToKnockAllWithAMissAsFirstRoll() {
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("-");
+            frameUnderTest.roll("1");
+
+            int totalSum = frameUnderTest.totalSum();
+
+            assertEquals(1, totalSum);
+        }
+
+        @Test
+        void failToKnockAllWithTwoMissedRoll() {
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("-");
+            frameUnderTest.roll("-");
+
+            int totalSum = frameUnderTest.totalSum();
+
+            assertEquals(0, totalSum);
+        }
+
+        @Test
+        void strike() {
+            BowlingFrame nextBowlingFrame = new BowlingFrame();
+            nextBowlingFrame.roll("1");
+            nextBowlingFrame.roll("1");
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("X");
+            frameUnderTest.nextFrame(nextBowlingFrame);
+
+            int totalSum = frameUnderTest.totalSum();
+
+            assertEquals(12, totalSum);
+        }
+
+        @Test
+        void twoStrike() {
+            BowlingFrame lastBowlingFrame = new BowlingFrame();
+            lastBowlingFrame.roll("1");
+            lastBowlingFrame.roll("1");
+            BowlingFrame nextFrameWithStrike = new BowlingFrame();
+            nextFrameWithStrike.roll("X");
+            nextFrameWithStrike.nextFrame(lastBowlingFrame);
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("X");
+            frameUnderTest.nextFrame(nextFrameWithStrike);
+
+            int totalSum = frameUnderTest.totalSum();
+
+            assertEquals(21, totalSum);
+        }
+
+        @Test
+        void spare() {
+            BowlingFrame nextBowlingFrame = new BowlingFrame();
+            nextBowlingFrame.roll("1");
+            nextBowlingFrame.roll("1");
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("3");
+            frameUnderTest.roll("/");
+            frameUnderTest.nextFrame(nextBowlingFrame);
+
+            int totalSum = frameUnderTest.totalSum();
+
+            assertEquals(11, totalSum);
+        }
     }
+    
+    @Nested
+    @IndicativeSentencesGeneration(separator = ",", generator = DisplayNameGenerator.ReplaceUnderscores.class)
+    class Erkennung_eines_Strikes {
+        @Test
+        void wenn_alle_pins_umgeworfen_wurden(){
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("X");
 
-    @Test
-    void totalSum_secondFailToKnockAll() {
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("2");
-        frameUnderTest.roll("2");
+            assertTrue(frameUnderTest.isStrike());
+        }
 
-        int totalSum = frameUnderTest.totalSum();
+        @Test
+        void wenn_nicht_alle_pins_umgeworfen_wurden(){
+            BowlingFrame frameUnderTest = new BowlingFrame();
+            frameUnderTest.roll("3");
 
-        assertEquals(4, totalSum);
+            assertFalse(frameUnderTest.isStrike());
+        }   
     }
-
-    @Test
-    void totalSum_failToKnockAllWithAMissAsFirstRoll() {
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("-");
-        frameUnderTest.roll("1");
-
-        int totalSum = frameUnderTest.totalSum();
-
-        assertEquals(1, totalSum);
-    }
-
-    @Test
-    void totalSum_failToKnockAllWithTwoMissedRoll() {
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("-");
-        frameUnderTest.roll("-");
-
-        int totalSum = frameUnderTest.totalSum();
-
-        assertEquals(0, totalSum);
-    }
-
-    @Test
-    void totalSum_strike() {
-        BowlingFrame nextBowlingFrame = new BowlingFrame();
-        nextBowlingFrame.roll("1");
-        nextBowlingFrame.roll("1");
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("X");
-        frameUnderTest.nextFrame(nextBowlingFrame);
-
-        int totalSum = frameUnderTest.totalSum();
-
-        assertEquals(12, totalSum);
-    }
-
-    @Test
-    void totalSum_twoStrike() {
-        BowlingFrame lastBowlingFrame = new BowlingFrame();
-        lastBowlingFrame.roll("1");
-        lastBowlingFrame.roll("1");
-        BowlingFrame nextFrameWithStrike = new BowlingFrame();
-        nextFrameWithStrike.roll("X");
-        nextFrameWithStrike.nextFrame(lastBowlingFrame);
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("X");
-        frameUnderTest.nextFrame(nextFrameWithStrike);
-
-        int totalSum = frameUnderTest.totalSum();
-
-        assertEquals(21, totalSum);
-    }
-
-    @Test
-    void totalSum_spare() {
-        BowlingFrame nextBowlingFrame = new BowlingFrame();
-        nextBowlingFrame.roll("1");
-        nextBowlingFrame.roll("1");
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("3");
-        frameUnderTest.roll("/");
-        frameUnderTest.nextFrame(nextBowlingFrame);
-
-        int totalSum = frameUnderTest.totalSum();
-
-        assertEquals(11, totalSum);
-    }
-
-    @Test
-    void isStrike_strikeIsGiven(){
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("X");
-
-        assertTrue(frameUnderTest.isStrike());
-    }
-
-    @Test
-    void isStrike_strikeIsNotGiven(){
-        BowlingFrame frameUnderTest = new BowlingFrame();
-        frameUnderTest.roll("3");
-
-        assertFalse(frameUnderTest.isStrike());
-    }
-
-
-
 }
