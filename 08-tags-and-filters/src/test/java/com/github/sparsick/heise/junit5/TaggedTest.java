6bc0d090b752ed07ab5c558b2b484a34db9e0860
Sandra Parsick <mail@sandra-parsick.de>
2023-04-17 16:16:21 +0200

    init tags and filter

---
+++ 08-tags-and-filters/src/test/java/com/github/sparsick/heise/junit5/TaggedTest.java
@@ -0,0 +1,12 @@
+package com.github.sparsick.heise.junit5;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TaggedTest {
+
+    @Test
+    void simpleTest() {
+        System.out.println("A simple test");
+    }
+}
