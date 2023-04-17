a378d4e04767a3aa0aa547810e28830ae0525d07
Sandra Parsick <mail@sandra-parsick.de>
2023-04-17 17:01:58 +0200

    tags sample

--- 08-tags-and-filters/src/test/java/com/github/sparsick/heise/junit5/TaggedTest.java
+++ 08-tags-and-filters/src/test/java/com/github/sparsick/heise/junit5/TaggedTest.java
@@ -1,12 +1,34 @@
 package com.github.sparsick.heise.junit5;
 
-
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
+@Tag("benchmarkDemo")
 public class TaggedTest {
 
     @Test
-    void simpleTest() {
+    void simpleTest(){
         System.out.println("A simple test");
     }
+
+
+    @Test
+    @Tag("benchmark")
+    void benchmarkTest(){
+        System.out.println("A benchmark test");
+    }
+
+    @Test
+    @Tags({@Tag("benchmark"), @Tag("slow")})
+    void slowBenchmarkTest() {
+        System.out.println("A slow benchmark test");
+    }
+
+    @Test
+    @Benchmark
+    void ownBenchmarkAnnotationTest() {
+        System.out.println("A benchmark test with custom annotation");
+    }
+
 }
